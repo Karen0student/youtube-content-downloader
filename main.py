0915803fa4727ff178f05657fa2158f3a006acec
Karen Arzumanyan <arzkaren4@gmail.com>
2024-07-13 14:28:18 +0400

    adding virtual environment + Bug fixes and improvements

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 import time
 import tkinter as tk
-
-
+from datetime import datetime
 
 def get_playlist_info(url):
     ydl_opts = {
@@ -22,7 +22,13 @@ def get_playlist_info(url):
             info = ydl.extract_info(url, download=False)
             return info
     except yt_dlp.utils.DownloadError as e:
-        print(f"Download error: {e}")
+        error_message = str(e).lower()
+        if 'sign in' in error_message or 'private' in error_message:
+            print("Authentication or special permissions required to access this content.")
+        elif '403' in error_message:
+            print("Access forbidden. This content may require authentication.")
+        else:
+            print(f"Download error: {e}")
     except yt_dlp.utils.ExtractorError as e:
         print(f"Extractor error: {e}")
     except yt_dlp.utils.UnsupportedError as e:
@@ -77,10 +83,10 @@ def download_youtube_video(url, download_type, download_path):
         
 
 def main():
+    playlist = []
     if os.path.exists(f"./youtube_link.txt"):
         with open('./youtube_link.txt', 'r') as file:
             playlist = file.readlines()
-            print(playlist)
     else:
         #playlist = str(sys.stdin.readline('Enter youtube playlist url and video count separated by one "space": '))
         playlist.append(str(input('Enter youtube playlist url and video count separated by one "space": ')))
@@ -88,9 +94,6 @@ def main():
     download_type = input("Do you want to download 'video' or 'audio'? (press Enter to use default 'audio'): ") or 'audio'
     
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")  # Run in headless mode
-    # chrome_options.add_argument("--disable-gpu")  # Disable GPU usage (optional)
-    # chrome_options.add_argument("--no-sandbox")  # Bypass OS security model (optional)
     chrome_options.binary_location = '/usr/bin/chromium-browser'  # Path to the Chromium browser
     driver = webdriver.Chrome(options=chrome_options)
     download_path = "./downloads"
@@ -99,12 +102,15 @@ def main():
     scrapped_path = "./scraped_urls.txt" # create folder to save urls
     if not os.path.exists(scrapped_path):
         os.makedirs(scrapped_path)
+        
     for index in range(len(playlist)):
         download_path = "./downloads" # reseting because adding ./downloads/{new_playlist}
         if playlist is None: # if end of file reached
             print("FINISH")
             break
-        parts = playlist[index].split() # playlist is list        
+        parts = playlist[index].split() # playlist is list
+        if not parts:
+            break
         playlist_url = str(parts[0])
         # create album folder by playlist name
         title = get_playlist_info(playlist_url)
@@ -119,15 +125,20 @@ def main():
         # Open YouTube video
         driver.get(playlist_url)
         with open(scrapped_path, 'a') as file: ######## optional
-                file.write(f"Playlist url: {playlist_url}\n")
+                file.write(f"Playlist url: {playlist_url} {str(datetime.now())}\n")
         
         for count in range(int(parts[1])):
             print("Wait for the page to load")
-            time.sleep(7)
+            time.sleep(4)
             print("working...")
-            # Click the share button
-            share_button = driver.find_element(By.CSS_SELECTOR, 'button[class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading"]')
-            share_button.click()
+            try:
+                # Check for sign-in requirement
+                if driver.find_element(By.CSS_SELECTOR, 'button[class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading"]'):
+                    driver.find_element(By.CSS_SELECTOR, 'button[class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading"]').click()
+            except:
+                print("ERROR, SKIPPING")
+                time.sleep(3) # for youtube's autonext algorithm
+                continue
 
             # Wait for the share menu to appear
             time.sleep(2)
@@ -154,6 +165,8 @@ def main():
             close_button = driver.find_element(By.CSS_SELECTOR, 'yt-icon[class="style-scope ytd-unified-share-panel-renderer"]')
             close_button.click()
             time.sleep(1)
+            if count == range(int(parts[1]) - 1): # Do not click the "Next" button if you have reached the end
+                break
             next_button = driver.find_element(By.CSS_SELECTOR, 'a[class="ytp-next-button ytp-button"]')
             next_button.click()
         
@@ -162,6 +175,7 @@ def main():
             
     # Close the browser
     driver.quit()
+    print("FINISHED")
 
 if __name__ == "__main__":
     main()
